1a667f335161f55098773270bab1d39fd07456b1
MasakiFujiwara <fmasaki2020@gmail.com>
2025-10-21 10:28:50 +0900

    Update .gitignore to include checkpoint directory and refactor training configuration in diffusion_model.py

--- diffusion_model.py
+++ diffusion_model.py
@@ -4,21 +4,54 @@ import torchvision
 import matplotlib.pyplot as plt
 from torchvision import transforms
 from torch.utils.data import DataLoader
-from torch.optim import Adam
+from torch.optim import AdamW
 import torch.nn.functional as F
 from torch import nn
-from tqdm import tqdm
+from PIL import Image
+# from tqdm import tqdm
 from model.utils.unet import UNet, UNetCond, UNetCondDeep
 from model.utils.diffuser import Diffuser
 import wandb
 import copy
 # from wandb import Alertlevel
 
-img_size = 32
-b_size = 128
-num_timeseteps = 1000
-epochs = 100
-lr = 1e-3
+from diffusers import UNet2DModel, DDPMScheduler, DDPMPipeline
+from diffusers.optimization import get_cosine_schedule_with_warmup
+
+from dataclasses import dataclass
+
+from accelerate import Accelerator
+from huggingface_hub import create_repo, upload_folder
+
+from tqdm.auto import tqdm
+import os
+
+@dataclass
+class TrainingConfig:
+    image_size = 64  # 生成する画像の解像度
+    train_batch_size = 64
+    eval_batch_size = 16  # 評価時にサンプリングする画像数
+    num_epochs = 100
+    gradient_accumulation_steps = 2
+    learning_rate = 1e-4
+    lr_warmup_steps = 500
+    save_image_epochs = 10
+    save_model_epochs = 10
+    mixed_precision = 'fp16'  # `no` は float32、`fp16` は自動混合精度
+    output_dir = 'checkpoint/diffuser-ddpm'  # モデルをローカルおよび HF Hub 上に保存するディレクトリ
+
+    push_to_hub = False  # 保存したモデルを HF Hub にアップロードするかどうか
+    hub_model_id = None  # HF Hub にアップロードする際のモデル ID（例: "username/model-name"）
+    hub_private_repo = False
+    project_name = "diffuser-ddpm"  # プロジェクト名（hub_model_id が指定されていない場合に使用）
+    overwrite_output_dir = True  # 再実行時に既存の出力ディレクトリを上書きするか
+    seed = 0
+    run_name = "ddpm-cifar10-64"  # Wandb の run 名
+
+config = TrainingConfig()
+
+num_timesteps = 1000
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
 
@@ -37,99 +70,246 @@ def show_images(imgs, rows=2, cols=10, labels=None):
     return fig
 
 preprocess = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    transforms.Normalize([0.5], [0.5]),
 ])
 # dataset = torchvision.datasets.CIFAR10(root='./data', transform=preprocess, download=True)
 dataset = torchvision.datasets.ImageFolder(root='~/../host_files/cifar10-64/train', transform=preprocess)
-dataloader = DataLoader(dataset, batch_size=b_size, shuffle=True, num_workers=2)
+train_dataloader = DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True, num_workers=2)
 
-diffuser = Diffuser(num_timesteps=num_timeseteps, device=device)
+# diffuser = Diffuser(num_timesteps=num_timeseteps, device=device)
 # model = UNet()
-model = UNetCondDeep(in_ch=3, num_labels=10)
-model = model.to(device)
-optimizer = Adam(model.parameters(), lr=lr)
-scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=lr, steps_per_epoch=len(dataloader), epochs=epochs)
-losses = []
+# model = UNetCondDeep(in_ch=3, num_labels=10)
+# model = model.to(device)
 
-config_dict = {
-    "dataset": "CIFAR10-64",
-    "model": model,
-    "epochs": epochs,
-    "batch_size": b_size,
-    "learning_rate": lr,
-    "num_timesteps": num_timeseteps,
-    "optimizer": optimizer,
-    "loss_function": "MSELoss",
-    "dataloader": dataloader,
-}
+model = UNet2DModel(
+    sample_size=config.image_size,  # the target image resolution
+    in_channels=3,  # the number of input channels, 3 for RGB images
+    out_channels=3,  # the number of output channels
+    layers_per_block=2,
+    block_out_channels=(128, 128, 256, 256, 512, 512),
+    down_block_types=(
+        "DownBlock2D",  # a regular ResNet downsampling block
+        "DownBlock2D",
+        "DownBlock2D",
+        "DownBlock2D",
+        "AttnDownBlock2D",  # a ResNet downsampling block with attention
+        "DownBlock2D",
+    ),
+    up_block_types=(
+        "UpBlock2D",  # a ResNet upsampling block with attention
+        "AttnUpBlock2D",
+        "UpBlock2D",  # a regular ResNet upsampling block
+        "UpBlock2D",
+        "UpBlock2D",
+        "UpBlock2D",
+    ),
+    # time_embedding_type="positional",  # use positional embeddings for time steps
+    # num_class_embeds=10,  # number of classes for class-conditional generation
+)
+sample_image = dataset[0][0].unsqueeze(0)
+# print('入力形状:', sample_image.shape)
+# print('出力形状:', model(sample_image, timestep=0)["sample"].shape)
 
-# ema initialization
-ema_decay = 0.995
-ema_model = copy.deepcopy(model).eval()
-for p in ema_model.parameters():
-    p.requires_grad_(False)
+noise_scheduler = DDPMScheduler(num_train_timesteps=num_timesteps)
 
-with wandb.init(project="DDPM_study", group="cifar10", name="deepU-mlp-ema-scheduler_64ver", config=config_dict):
+optimizer = AdamW(model.parameters(), lr=config.learning_rate)
 
-    for epoch in range(epochs):
-        loss_sum = 0.0
-        cnt = 0
+lr_scheduler = get_cosine_schedule_with_warmup(
+    optimizer,
+    num_warmup_steps=config.lr_warmup_steps,
+    num_training_steps=config.num_epochs * len(train_dataloader)
+)
 
-        for imgs, labels in tqdm(dataloader):
+def make_grid(images, rows, cols):
+    w, h = images[0].size
+    grid = Image.new('RGB', size=(cols*w, rows*h))
+    for i, image in enumerate(images):
+        grid.paste(image, box=(i%cols*w, i//cols*h))
+    return grid
 
-            # x = imgs.clone()
-            # imgs = [diffuser.reverse2img(x[i]) for i in range(128)]
-            # show_images(imgs, labels=labels)
+def evaluate(config, epoch, pipeline):
+    # ランダムノイズから画像をサンプルします（これは逆拡散プロセスです）。
+    # デフォルトのパイプライン出力型は `List[PIL.Image]` です
+    # images = pipeline(
+    #     batch_size = config.eval_batch_size,
+    #     generator=torch.manual_seed(config.seed),
+    # )["sample"]
+    images = pipeline(
+        batch_size = config.eval_batch_size,
+        generator=torch.manual_seed(config.seed),
+    ).images
 
-            optimizer.zero_grad()
-            x = imgs.to(device)
-            labels = labels.to(device)
-            t = torch.randint(1, num_timeseteps+1, (len(x), ), device=device)
+    # 画像をグリッドに配置
+    image_grid = make_grid(images, rows=4, cols=4)
 
-            x_noisy, noise = diffuser.add_noise(x, t)
-            noise_pred = model(x_noisy, t, labels)
-            loss = F.mse_loss(noise, noise_pred)
+    # 画像を保存
+    test_dir = os.path.join(config.output_dir, "samples")
+    os.makedirs(test_dir, exist_ok=True)
+    image_grid.save(f"{test_dir}/{epoch:04d}.png")
 
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
+    # Wandb に画像をログ
+    wandb.log({"samples": wandb.Image(image_grid)}, step=epoch)
 
-            with torch.no_grad():
-                msd = model.state_dict()
-                for k, ema_v in ema_model.state_dict().items():
-                    model_v = msd[k].detach()
-                    ema_v.copy_(ema_v * ema_decay + (1.0 - ema_decay) * model_v)
+def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
+    # Accelerator と Wandb ログの初期化
+    accelerator = Accelerator(
+        mixed_precision=config.mixed_precision,
+        gradient_accumulation_steps=config.gradient_accumulation_steps,
+        log_with="wandb"
+    )
+    if accelerator.is_main_process:
+        if config.push_to_hub:
+            repo_id = create_repo(
+                repo_id=config.hub_model_id or config.project_name,
+                exist_ok=True
+            ).repo_id
+        accelerator.init_trackers("DDPM_study", config={"name": config.run_name, "config": config.__dict__})
 
-            loss_sum += loss.item()
-            cnt += 1
+    # すべてを prepare する
+    # 順序に特別な意味はありませんが、prepare に渡した順番でアンパックしてください。
+    model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+        model, optimizer, train_dataloader, lr_scheduler
+    )
 
-        loss_avg = loss_sum / cnt
-        wandb.log({"loss": loss_avg, "learning_rate": scheduler.get_last_lr()[0]}, step=epoch)
-        losses.append(loss_avg)
-        print(f"Epoch {epoch}, Loss: {loss_avg}")
-        # if (epoch + 1) % 10 == 0:
-        #     torch.save(ema_model.state_dict(), f'checkpoint/deepU-mlp-ema-scheduler/model_cifar10_epoch{epoch+1}.pth')
+    global_step = 0
 
-        if (epoch + 1) % 5 == 0:
-            # 0~9を2回繰り返したラベルを作成
-            sample_labels = torch.tensor([i for i in range(10)] * 2, device=device)
-            imgs_sampled, sample_labels = diffuser.sample(ema_model, labels=sample_labels)
-            # 画像グリッドを作成し、wandbに登録
-            fig = show_images(imgs_sampled, labels=sample_labels)
-            wandb.log({"samples_grid": wandb.Image(fig, caption=f"Epoch {epoch+1} grid")}, step=epoch+1)
-            plt.close(fig)
-            # 個別画像も登録したい場合は下記を有効化
-            # wandb_images = [wandb.Image(img, caption=str(lbl.item())) for img, lbl in zip(imgs_sampled, sample_labels)]
-            # wandb.log({"samples": wandb_images}, step=epoch)
+    # 学習ループ開始
+    for epoch in range(config.num_epochs):
+        progress_bar = tqdm(total=len(train_dataloader), disable=not accelerator.is_local_main_process)
+        progress_bar.set_description(f"epoch {epoch}")
 
-wandb.finish()
+        for step, batch in enumerate(train_dataloader):
+            clean_images = batch[0]
+            # 画像に加えるノイズをサンプリング
+            noise = torch.randn(clean_images.shape).to(clean_images.device)
+            bs = clean_images.shape[0]
 
-plt.plot(losses)
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.show()
+            # 各画像に対してランダムなタイムステップをサンプリング
+            timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (bs,), device=clean_images.device).long()
 
-imgs, labels = diffuser.sample(model)
-fig = show_images(imgs, labels=labels)
-plt.show()
+            # 各タイムステップに応じてクリーン画像にノイズを加える
+            # （これがフォワード拡散プロセスです）
+            noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+
+            with accelerator.accumulate(model):
+                # ノイズ残差を予測
+                noise_pred = model(noisy_images, timesteps)["sample"]
+                loss = F.mse_loss(noise_pred, noise)
+                accelerator.backward(loss)
+
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+
+            progress_bar.update(1)
+            logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "step": global_step}
+            progress_bar.set_postfix(**logs)
+            accelerator.log(logs, step=global_step)
+            global_step += 1
+
+        # 各エポック後にサンプル画像を評価用に生成してモデルを保存
+        if accelerator.is_main_process:
+            pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
+
+            if (epoch + 1) % config.save_image_epochs == 0 or epoch == config.num_epochs - 1:
+                evaluate(config, epoch, pipeline)
+
+            if (epoch + 1) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1:
+                if config.push_to_hub:
+                    pipeline.save_pretrained(config.output_dir)
+                    upload_folder(
+                        repo_id=repo_id,
+                        folder_path=config.output_dir,
+                        commit_message=f"epoch {epoch}",
+                        ignore_patterns=["step_*", "epoch_*"],
+                    )
+                else:
+                    pipeline.save_pretrained(config.output_dir)
+
+train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler)
+
+# losses = []
+
+# config_dict = {
+#     "dataset": "CIFAR10-64",
+#     "model": model,
+#     "epochs": epochs,
+#     "batch_size": b_size,
+#     "learning_rate": lr,
+#     "num_timesteps": num_timeseteps,
+#     "optimizer": optimizer,
+#     "loss_function": "MSELoss",
+#     "dataloader": dataloader,
+# }
+
+# # ema initialization
+# ema_decay = 0.995
+# ema_model = copy.deepcopy(model).eval()
+# for p in ema_model.parameters():
+#     p.requires_grad_(False)
+
+# with wandb.init(project="DDPM_study", group="cifar10", name="deepU-mlp-ema-scheduler_64ver", config=config_dict):
+
+#     for epoch in range(epochs):
+#         loss_sum = 0.0
+#         cnt = 0
+
+#         for imgs, labels in tqdm(dataloader):
+
+#             # x = imgs.clone()
+#             # imgs = [diffuser.reverse2img(x[i]) for i in range(128)]
+#             # show_images(imgs, labels=labels)
+
+#             optimizer.zero_grad()
+#             x = imgs.to(device)
+#             labels = labels.to(device)
+#             t = torch.randint(1, num_timeseteps+1, (len(x), ), device=device)
+
+#             x_noisy, noise = diffuser.add_noise(x, t)
+#             noise_pred = model(x_noisy, t, labels)
+#             loss = F.mse_loss(noise, noise_pred)
+
+#             loss.backward()
+#             optimizer.step()
+#             scheduler.step()
+
+#             with torch.no_grad():
+#                 msd = model.state_dict()
+#                 for k, ema_v in ema_model.state_dict().items():
+#                     model_v = msd[k].detach()
+#                     ema_v.copy_(ema_v * ema_decay + (1.0 - ema_decay) * model_v)
+
+#             loss_sum += loss.item()
+#             cnt += 1
+
+#         loss_avg = loss_sum / cnt
+#         wandb.log({"loss": loss_avg, "learning_rate": scheduler.get_last_lr()[0]}, step=epoch)
+#         losses.append(loss_avg)
+#         print(f"Epoch {epoch}, Loss: {loss_avg}")
+#         # if (epoch + 1) % 10 == 0:
+#         #     torch.save(ema_model.state_dict(), f'checkpoint/deepU-mlp-ema-scheduler/model_cifar10_epoch{epoch+1}.pth')
+
+#         if (epoch + 1) % 5 == 0:
+#             # 0~9を2回繰り返したラベルを作成
+#             sample_labels = torch.tensor([i for i in range(10)] * 2, device=device)
+#             imgs_sampled, sample_labels = diffuser.sample(ema_model, labels=sample_labels)
+#             # 画像グリッドを作成し、wandbに登録
+#             fig = show_images(imgs_sampled, labels=sample_labels)
+#             wandb.log({"samples_grid": wandb.Image(fig, caption=f"Epoch {epoch+1} grid")}, step=epoch+1)
+#             plt.close(fig)
+#             # 個別画像も登録したい場合は下記を有効化
+#             # wandb_images = [wandb.Image(img, caption=str(lbl.item())) for img, lbl in zip(imgs_sampled, sample_labels)]
+#             # wandb.log({"samples": wandb_images}, step=epoch)
+
+# wandb.finish()
+
+# plt.plot(losses)
+# plt.xlabel('Epoch')
+# plt.ylabel('Loss')
+# plt.show()
+
+# imgs, labels = diffuser.sample(model)
+# fig = show_images(imgs, labels=labels)
+# plt.show()
